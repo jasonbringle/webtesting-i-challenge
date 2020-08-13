f87cab0bc8e4b0127de7dd157dd0f50ac3460408
jasonbringle <jasonbringle@gmail.com>
2020-08-12 23:34:48 -0700

    MVP achieved

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -20,7 +20,24 @@ function success(item) {
 }
 
 function fail(item) {
-  return { ...item };
+  if(item.hasOwnProperty("enhancement")){
+    if(item.enhancement < 15){
+      item.durability = item.durability - 5
+      return item
+      }else{
+        if(item.enhancement >= 15  && item.enhancement <= 16){
+          item.durability = item.durability - 10
+          return item
+        }else{
+          if(item.enhancement > 16){
+            item.enhancement = item.enhancement - 1
+            return item
+          }}
+          return item
+      }
+  } else {
+    return "Item is not an object"
+  }
 }
 
 function repair(item) {
@@ -30,8 +47,8 @@ function repair(item) {
       return item
       } else {
         return 'Durability is not between 0 and 100.'
-      }}{
-        return 'The durability field is missing..'
+      }} else {
+        return  'The durability field is missing..'
       }
 }
 
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,9 +1,10 @@
-const {enhancer, repair, success} = require('./enhancer.js');
+const {enhancer, repair, success, fail} = require('./enhancer.js');
 
 describe("enhancers", () => {
     describe('repair', () => {
         
         test('checks that argument is an object with proper keys', () => {
+
                 let actual = repair({
                     name: 'Jim',
                     durability: 5,
@@ -18,29 +19,21 @@ describe("enhancers", () => {
 
             expect(actual).toMatchObject(expected)
 
-                let array = repair([
-                    'Jim',
-                    5,
-                    34
-                ])
-
+                let array = repair(['Jim',5,34])
                 let objectError = 'The durability field is missing..'
 
             expect(array).toBe(objectError)
-
         })
 
         test('checks object for durability key', () => {
+
                 let actual = repair({
                     name: 'Jim',
                     enhancement: 34
                 })
-
                 let error = 'The durability field is missing..'
 
             expect(actual).toBe(error)
-
-
         });
 
         test('checks that durability value is a number between 0 and 100', () => {
@@ -49,7 +42,6 @@ describe("enhancers", () => {
                     durability: 101,
                     enhancement: 34
                 })
-                
                 let error = 'Durability is not between 0 and 100.'
 
             expect(tooHighDurability).toStrictEqual(expect.stringContaining(error))
@@ -67,7 +59,6 @@ describe("enhancers", () => {
                     durability: 50,
                     enhancement: 23
                 })
-                
                 let fixedDurability = {
                     name: 'Jim',
                     durability: 100,
@@ -80,25 +71,84 @@ describe("enhancers", () => {
 
     describe("success", ()=> {
         test("checks if argument is an object", ()=> {
+
                 let actual = []
                 let error = "This argument is not an object"
+
             expect(success(actual)).toBe(error)
         })
 
         test("checks if enhancement is a number between 0 and 20", () => {
                 let biggerThanTwenty = {
-                        name: 'Jim',
-                        durability: 100,
-                        enhancement: 23
+                    name: 'Jim',
+                    durability: 100,
+                    enhancement: 23
                 }
-
                 let expected = "Enhancement doesn't exist or is too high or a negative number"
+
             expect(success(biggerThanTwenty)).toStrictEqual(expect.stringContaining(expected))
         })
+    })
+
+    describe("fail", ()=> {
+        test("checks if object is being submitted to function",  () => {
+                let actual = {
+                    name: 'Jim',
+                    durability: 100,
+                }
+                let expected = "Item is not an object";
+
+            expect(fail(actual)).toStrictEqual(expect.stringContaining(expected))
+        })
+
+        test("If the item's enhancement is less than 15, the durability of the item is decreased by 5", () => {
+
+                let enhancementtLessThan = {
+                    name: 'Jim',
+                    durability: 100,
+                    enhancement: 14
+                }
+                let expected = {
+                    name: 'Jim',
+                    durability: 95,
+                    enhancement: 14
+                }
+
+            expect(fail(enhancementtLessThan)).toStrictEqual(expected)
+        })
+
+        test("If the item's enhancement is 15 or more, the durability of the item is decreased by 10", ()=> {
+
+                let enhancementtMoreThan = {
+                    name: 'Jim',
+                    durability: 100,
+                    enhancement: 16
+                }
+                let expected = {
+                    name: 'Jim',
+                    durability: 90,
+                    enhancement: 16
+                }
+
+            expect(fail(enhancementtMoreThan)).toStrictEqual(expected);
+        })
         
-        test.todo("")
-        test.todo("")
-        test.todo("")
+        test("If the item's enhancement level is greater than 16, the enhancement level decreases by 1 (17 goes down to 16, 18 goes down to 17)", () => {
+
+                let actual = {
+                    name: 'Jim',
+                    durability: 100,
+                    enhancement: 17
+                }
+                let expected = {
+                    name: 'Jim',
+                    durability: 100,
+                    enhancement: 16
+                }
+            
+            expect(fail(actual)).toStrictEqual(expected)
+
+        })
     })
 })
 
